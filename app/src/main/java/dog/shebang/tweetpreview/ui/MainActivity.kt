2f7be8f1b7d2ca61b92c4d555bae0db59e0a4a71
ShebangDog <e1n18088@oit.ac.jp>
2020-11-05 02:53:09 +0900

    TopAppBarを追加する

--- app/src/main/java/dog/shebang/tweetpreview/ui/MainActivity.kt
+++ app/src/main/java/dog/shebang/tweetpreview/ui/MainActivity.kt
@@ -2,12 +2,16 @@ package dog.shebang.tweetpreview.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.navigation.compose.*
 import dog.shebang.tweetpreview.theme.TweetPreviewTheme
@@ -31,24 +35,36 @@ class MainActivity : AppCompatActivity() {
 fun Navigation() {
     val navController = rememberNavController()
 
-    Scaffold(bottomBar = {
-        BottomNavigation {
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentRoute = navBackStackEntry?.arguments?.getString(KEY_ROUTE)
+    Scaffold(
+        topBar = {
+            TopAppBar {
+                Row(
+                    modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
 
-            listOf(Screen.Home, Screen.Detail).forEach { screen ->
-                BottomNavigationItem(
-                    icon = { Icon(asset = screen.icon) },
-                    selected = currentRoute == screen.name,
-                    onClick = {
-                        navController.popBackStack()
-
-                        if (screen.name != currentRoute) navController.navigate(screen.name)
-                    }
-                )
+                    IconButton(onClick = {}, icon = { Icon(asset = Icons.Default.Person) })
+                }
             }
-        }
-    }) { innerPadding ->
+        },
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.arguments?.getString(KEY_ROUTE)
+
+                listOf(Screen.Home, Screen.Detail).forEach { screen ->
+                    BottomNavigationItem(
+                        icon = { Icon(asset = screen.icon) },
+                        selected = currentRoute == screen.name,
+                        onClick = {
+                            navController.popBackStack()
+
+                            if (screen.name != currentRoute) navController.navigate(screen.name)
+                        }
+                    )
+                }
+            }
+        }) { innerPadding ->
 
         NavHost(navController = navController, startDestination = Screen.Home.name) {
             composable(Screen.Home.name) { HomeScreen() }
